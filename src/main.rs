2c092d91d5ba061bc8ff8b3a15fb30c6164f1b60
Dmytrii Nagirniak <dnagir@gmail.com>
2018-07-22 00:41:48 +1000

    Generate the CF using compile-time templates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+struct SecurityGroup {
+    name: String,
+    description: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let groups = vec![
+        SecurityGroup {
+            name: "asd".to_string(),
+            description: "ddd".to_string(),
+        },
+        SecurityGroup {
+            name: "bc".to_string(),
+            description: "def".to_string(),
+        },
+    ];
+
+    let resources: Vec<String> = groups
+        .iter()
+        .map(|sg| {
+            format!(
+                include_str!("sg.tmpl"),
+                name = sg.name,
+                description = sg.description
+            )
+        })
+        .collect();
+    let outputs: Vec<String> = groups
+        .iter()
+        .map(|sg| {
+            format!(
+                include_str!("sg-out.tmpl"),
+                name = sg.name,
+                description = sg.description
+            )
+        })
+        .collect();
+
+    println!(
+        include_str!("main.tmpl"),
+        resources = resources.join("\n"),
+        outputs = outputs.join("\n")
+    );
 }
